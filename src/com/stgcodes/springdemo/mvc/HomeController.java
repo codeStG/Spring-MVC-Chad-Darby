d1a7e33f7aa79eddee1351bcbc91d1b65c11b248
Torres <getorres@teksystems.com>
2022-06-07 12:05:05 -0500

    Creating a Controller

---
+++ src/com/stgcodes/springdemo/mvc/HomeController.java
@@ -0,0 +1,13 @@
+package com.stgcodes.springdemo.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String renderView() {
+		return "main-menu";
+	}
+}
